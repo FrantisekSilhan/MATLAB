68c288ce31e29fcfde0c494d2ee0d103ede480f0
František Šilhán <frantisek@slhn.cz>
2025-11-18 15:48:17 +0100

    add zderivuj.m

--- NTI-SEM1/009/sem1009.m
+++ NTI-SEM1/009/sem1009.m
@@ -4,3 +4,10 @@ format compact;
 
 Newton(0.1, @log, 1e-6, 100, 1e-6)
 Newton(2, @(x) (x.^2 - 2), 1e-6, 100, 1e-6)
+
+x = -4:0.01:3;
+y = sin(x);
+z = zderivuj(x, y);
+plot(x, z);
+hold on;
+plot(x, cos(x));
