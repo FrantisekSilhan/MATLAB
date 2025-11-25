ea698f3c7e4b1f87fb84908827197e6054a1dfa8
František Šilhán <frantisek@slhn.cz>
2025-11-25 15:01:06 +0100

    add sem1010

---
+++ NTI-SEM1/010/Integral.m
@@ -0,0 +1,7 @@
+function I = Integral(x, y)
+  I = 0;
+  for i = 1: length(x) - 1
+    I = I + (x(i+1) - x(i)) * (y(i) + y(i+1));
+  end
+  I = I / 2;
+end
---
+++ NTI-SEM1/010/sem1010.m
@@ -0,0 +1,6 @@
+x = 0:0.01:1;
+y = sin(x);
+
+Integral(x, y)
+
+integral(@sin, 0, 1)
