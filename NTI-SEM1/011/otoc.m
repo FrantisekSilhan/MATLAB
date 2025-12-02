68d5af0db93c17af7af645697371c1d538d5281a
František Šilhán <frantisek@slhn.cz>
2025-12-02 15:50:47 +0100

    feat(SEM1011): add degrees/radians option in otoc.m

--- NTI-SEM1/011/otoc.m
+++ NTI-SEM1/011/otoc.m
@@ -1,3 +1,13 @@
-function Y = otoc(X, alpha)
-    Y = [X(1, :) * cosd(alpha) - X(2, :) * sind(alpha); X(1, :) * sind(alpha) + X(2, :) * cosd(alpha)];
+function Y = otoc(X, alpha, unit)
+    arguments
+        X
+        alpha
+        unit (1,1) string {mustBeMember(unit, ["degrees", "radians"])} = "degrees"
+    end
+    if (unit == "radians")
+        Y = [X(1, :) * cos(alpha) - X(2, :) * sin(alpha); X(1, :) * sin(alpha) + X(2, :) * cos(alpha)];
+    end
+    if (unit == "degrees")
+        Y = [X(1, :) * cosd(alpha) - X(2, :) * sind(alpha); X(1, :) * sind(alpha) + X(2, :) * cosd(alpha)];
+    end
 end
